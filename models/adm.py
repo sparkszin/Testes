cc84b48c4c3d7c0d4951d9c248d63792910eea55
ZER0 <fabiolandeiro12@gmail.com>
2024-11-22 16:44:10 +0000

    novo comentario

--- models/adm.py
+++ models/adm.py
@@ -1,6 +1,7 @@
 from datetime import date
 from odoo import api, fields, models
 
+# adm comentario como funciona?
 class Adm(models.Model):
     _name = "adm"
     _description = "Modelo para Criar Administração"
--- models/crimes.py
+++ models/crimes.py
@@ -1,7 +1,7 @@
 from datetime import date
 from odoo import api, fields, models
 
-# Modelo dos Crimes
+# Modelo dos Crimes 123
 class Crimes(models.Model):
     _name = "crimes"
     _description = "Modelo para Criar Crimes"
