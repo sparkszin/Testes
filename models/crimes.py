c56020f79719e37ae6375bc434a8e11a7420561b
ZER0 <fabiolandeiro12@gmail.com>
2024-11-22 16:34:03 +0000

    comentario

--- models/crimes.py
+++ models/crimes.py
@@ -1,6 +1,7 @@
 from datetime import date
 from odoo import api, fields, models
 
+# Modelo dos Crimes
 class Crimes(models.Model):
     _name = "crimes"
     _description = "Modelo para Criar Crimes"
